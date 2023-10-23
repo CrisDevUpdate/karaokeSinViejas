fcecdb1b1860b5ef51131e07f6f0816b8190fc44
CrisDevUpdate <cris.dev.update@gmail.com>
2023-10-22 22:11:36 -0430

    algo

---
+++ Code.js
@@ -0,0 +1,8 @@
+alert ("HELLO VIRGENES")
+
+if (confirm ('Para acceder a este sitio usted no debe ser una femina, de en\ "OK" si es\ el caso. Si en caso dado sí es una femina y de igual forma le da en \"ok"\ no pasa nada, igual todas mienten ' )) {
+alert ('Grave error, ahora tienes que mandar 100$ a mi cuenta bancaría o te explota la casa.');
+}
+else {
+alert ("JA! Espanté otra.");
+}
